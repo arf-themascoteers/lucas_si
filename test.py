71b351a85e880a6955363c9d8f0450bc4bb50aeb
Arif <arif@themascoteers.com>
2023-11-09 00:37:23 +1100

    done

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import spec_utils
 
 if __name__ == "__main__":
     column_groups = [
-        spec_utils.get_wavelengths()
+        spec_utils.get_wavelengths(),
+        spec_utils.get_wavelengths() + ["savi"],
     ]
 
     c = Evaluator(prefix="t2", folds=10, algorithms=["ann_savi_only","ann_savi"], column_groups=column_groups)
